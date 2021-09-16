31657128e9885388ea42d4b7b45d275f38964dfd
Anna Marín <83779679+Amc210@users.noreply.github.com>
2021-09-16 19:41:37 +0200

    Update sql-corregido.sql

--- ModeloProyecto/SQL/sql-corregido.sql
+++ ModeloProyecto/SQL/sql-corregido.sql
@@ -1,17 +1,17 @@
-/* SQL corregido a fecha 9 sept*/
+/* SQL corregido a fecha 16 sept*/
 
 create database LFG;
 use LFG;
 
-create table juegos (id_juego int primary key auto_increment, nombre varchar(100), imagen_juego varchar(255));
-create table grupos (id_grupo int primary key auto_increment, nombre varchar(40), creador varchar(40), id_juego int, foreign key (id_juego) REFERENCES juegos (id_juego));
+create table juegos (id_juego int primary key auto_increment not null, nombre varchar(100) not null, imagen_juego varchar(255) not null);
+create table grupos (id_grupo int primary key auto_increment not null, nombre varchar(40) not null, creador varchar(40) not null, tipo int not null, descripcion varchar(255) not null, id_juego int not null, foreign key (id_juego) REFERENCES juegos (id_juego) on delete cascade on update cascade);
 
-create table usuarios (id_usuario int primary key auto_increment, nick varchar(20), nombre varchar(40), tipo int, email varchar(50));
+create table usuarios (id_usuario int primary key auto_increment not null, nick varchar(20) not null, nombre varchar(40) not null, email varchar(50) not null);
 
-create table mensajes_privados (id_mensaje_privado int primary key auto_increment, contenido varchar(255), timestamp timestamp, id_emisor int, id_receptor int, key (id_emisor), key (id_receptor), foreign key (id_emisor) REFERENCES usuarios(id_usuario), foreign key (id_receptor) REFERENCES usuarios(id_usuario));
+create table mensajes_privados (id_mensaje_privado int primary key auto_increment not null, contenido varchar(255) not null, timestamp timestamp not null, id_emisor int not null, id_receptor int not null, foreign key (id_emisor) REFERENCES usuarios(id_usuario) on delete cascade on update cascade, foreign key (id_receptor) REFERENCES usuarios(id_usuario) on delete cascade on update cascade);
 
-create table mensajes (id_mensaje int primary key auto_increment, contenido varchar(255), timestamp timestamp, id_grupo int, key (id_grupo), foreign key (id_grupo) REFERENCES grupos(id_grupo));
+create table mensajes (id_mensaje int primary key auto_increment not null, contenido varchar(255) not null, timestamp timestamp not null, id_grupo int not null, foreign key (id_grupo) REFERENCES grupos(id_grupo) on delete cascade on update cascade);
 
-create table usuarios_agregan_amigos (id_usuario_agrega_amigos int primary key auto_increment, id_usuario int, id_amigo int, foreign key (id_usuario) REFERENCES usuarios (id_usuario), foreign key (id_amigo) REFERENCES usuarios(id_usuario));
+create table usuarios_agregan_amigos (id_usuario_agrega_amigos int primary key auto_increment not null, id_usuario int not null, id_amigo int not null, foreign key (id_usuario) REFERENCES usuarios (id_usuario), foreign key (id_amigo) REFERENCES usuarios(id_usuario) on delete cascade on update cascade);
 
-create table usuarios_pertenecen_a_grupos (id_usuario_grupo int primary key auto_increment, id_grupo int, id_usuario int, foreign key (id_grupo) REFERENCES grupos (id_grupo), foreign key (id_usuario) REFERENCES usuarios(id_usuario));
+create table usuarios_pertenecen_a_grupos (id_usuario_grupo int primary key auto_increment not null, id_grupo int not null, id_usuario int not null, foreign key (id_grupo) REFERENCES grupos (id_grupo), foreign key (id_usuario) REFERENCES usuarios(id_usuario) on delete cascade on update cascade);
